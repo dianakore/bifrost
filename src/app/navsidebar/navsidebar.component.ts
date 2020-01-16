728b9b4a4a7f02a6e57dda6afadeb272a93bba18
dianaKore <Briciola2015gatto>
2020-01-16 17:54:46 +0100

    new project setup

---
+++ src/app/navsidebar/navsidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navsidebar',
+  templateUrl: './navsidebar.component.html',
+  styleUrls: ['./navsidebar.component.scss']
+})
+export class NavsidebarComponent {
+  opened: boolean;
+  events: string[] = [];
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
